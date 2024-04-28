d5ba013cc919d6c2114bc155e20db155941d95af
Kristian Perez <kpere192@fiu.edu>
2024-04-28 15:42:58 -0400

    Made all piece objs share the squares array from the board obj, made a get_moves function that finds all possible moves for a pawn

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,8 @@ class Board
   LENGTH = 8
   WIDTH = 8
 
+  attr_accessor :squares
+
   def initialize(length = LENGTH, width = WIDTH)
     @length = length
     @width = width
@@ -18,28 +20,28 @@ class Board
 
   def init_pieces
     @squares[1].each_with_index do |piece, file|
-      @squares[1][file] = Pawn.new("white", [1, file], self)
+      @squares[1][file] = Pawn.new("white", [1, file], @squares)
     end
-    @squares[0][0] = Rook.new("white", [0,0], self)
-    @squares[0][1] = Knight.new("white", [0,1], self)
-    @squares[0][2] = Bishop.new("white", [0,2], self)
-    @squares[0][3] = Queen.new("white", [0,3], self)
-    @squares[0][4] = King.new("white", [0,4], self)
-    @squares[0][5] = Bishop.new("white", [0,5], self)
-    @squares[0][6] = Knight.new("white", [0,6], self)
-    @squares[0][7] = Rook.new("white", [0,7], self)
+    @squares[0][0] = Rook.new("white", [0,0], @squares)
+    @squares[0][1] = Knight.new("white", [0,1], @squares)
+    @squares[0][2] = Bishop.new("white", [0,2], @squares)
+    @squares[0][3] = Queen.new("white", [0,3], @squares)
+    @squares[0][4] = King.new("white", [0,4], @squares)
+    @squares[0][5] = Bishop.new("white", [0,5], @squares)
+    @squares[0][6] = Knight.new("white", [0,6], @squares)
+    @squares[0][7] = Rook.new("white", [0,7], @squares)
 
     @squares[6].each_with_index do |piece, file|
-      @squares[6][file] = Pawn.new("black", [6, file], self)
+      @squares[6][file] = Pawn.new("black", [6, file], @squares)
     end
-    @squares[7][0] = Rook.new("black", [7,0], self)
-    @squares[7][1] = Knight.new("black", [7,1], self)
-    @squares[7][2] = Bishop.new("black", [7,2], self)
-    @squares[7][3] = Queen.new("black", [7,3], self)
-    @squares[7][4] = King.new("black", [7,4], self)
-    @squares[7][5] = Bishop.new("black", [7,5], self)
-    @squares[7][6] = Knight.new("black", [7,6], self)
-    @squares[7][7] = Rook.new("black", [7,7], self)
+    @squares[7][0] = Rook.new("black", [7,0], @squares)
+    @squares[7][1] = Knight.new("black", [7,1], @squares)
+    @squares[7][2] = Bishop.new("black", [7,2], @squares)
+    @squares[7][3] = Queen.new("black", [7,3], @squares)
+    @squares[7][4] = King.new("black", [7,4], @squares)
+    @squares[7][5] = Bishop.new("black", [7,5], @squares)
+    @squares[7][6] = Knight.new("black", [7,6], @squares)
+    @squares[7][7] = Rook.new("black", [7,7], @squares)
   end
 
   def print_board
--- lib/piece.rb
+++ lib/piece.rb
@@ -1,14 +1,14 @@
 class Piece
-  attr_accessor :icon
+  attr_accessor :icon, :color, :board
 
   def initialize(color, icon, pos = [0,0], board, type)
     color = color.downcase
-    color == "white" || color == "black" ? @color = color : color = "NA"
+    color == "white" || color == "black" ? @color = color : @color = "NA"
     color == "white" ? @icon = "\e[37m\e[1m#{icon}\e[22m" : @icon = "\e[30m#{icon}"
     @rank = pos[0]
     @file = pos[1]
     @type = type
-    @board = board
+    @@board = board
   end
   def to_s
     "#{@color} #{@type} at #{(@rank.ord + "a".ord).chr}#{@file}"
@@ -17,37 +17,54 @@ end
 
 class Pawn < Piece
   def initialize(color, pos, board)
-    super(color, "p", pos, "pawn")
+    super(color, "p", pos, board, "pawn")    
+  end
+
+  def get_moves
+    moves = []
+    if color == "white" then direction = 1 else direction = -1 end
+    if color == "white" && @rank == 1 then moves.push([@rank + 2, @file]) end
+    if color == "black" && @rank == 1 then moves.push([@rank - 2, @file]) end
+    if(@rank + direction <= @@board.length)
+      if @@board[@rank + direction][@file] == nil then moves.push([@rank + direction, @file]) end
+      if(@file + 1 <= @@board[0].length && @@board[@rank + direction][@file + 1] != nil)
+        if @@board[@rank + direction][@file + 1].color != @color then moves.push([@rank + direction, @file + 1]) end
+      end
+      if(@file - 1 <= @@board[0].length && @@board[@rank + direction][@file - 1] != nil)
+        if @@board[@rank + direction][@file - 1].color != @color then moves.push([@rank + direction, @file - 1]) end
+      end
+    end
+    moves
   end
 end
 
 class Rook < Piece
   def initialize(color, pos, board)
-    super(color, "r", pos, "rook")
+    super(color, "r", pos, board, "rook")
   end
 end
 
 class Knight < Piece
   def initialize(color, pos, board)
-    super(color, "k", pos, "knight")
+    super(color, "k", pos, board, "knight")
   end
 end
 
 class Bishop < Piece
   def initialize(color, pos, board)
-    super(color, "b", pos, "bishop")
+    super(color, "b", pos, board, "bishop")
   end
 end
 
 class Queen < Piece
   def initialize(color, pos, board)
-    super(color, "q", pos, "queen")
+    super(color, "q", pos, board, "queen")
   end
 end
 
 class King < Piece
   def initialize(color, pos, board)
-    super(color, "K", pos, "king")
+    super(color, "K", pos, board, "king")
     color == "white" ? @icon = "\e[37m\e[1m\e[4m#{icon}\e[24m\e[22m" : @icon = "\e[30m\e[4m#{icon}\e[24m\e[22m"
   end
 end
