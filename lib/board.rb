98d7d33db70251fc845bf1428ba8c1458deae269
Kristian Perez <kpere192@fiu.edu>
2024-04-29 15:05:48 -0400

    Added pieces instance var to player obj. Keeps track of what pieces the player owns, wrote a get_pieces function for player class that gets all its colored pieces into @pieces var.

--- lib/board.rb
+++ lib/board.rb
@@ -43,14 +43,8 @@ class Board
     @squares[7][6] = Knight.new("black", [7,6], @squares)
     @squares[7][7] = Rook.new("black", [7,7], @squares)
 
-    @squares.each do |row|
-      row.each do |piece|
-        if(piece != nil)
-          p "#{piece.to_s}: #{piece.get_moves}"
-        end
-      end
-    end
-    
+    @players[0].get_pieces(@squares)
+    @players[1].get_pieces(@squares)    
   end
 
   def print_board
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,24 @@
 class Player
 
+  attr_accessor :pieces, :color
+
   def initialize(color)
     color = color.downcase
-    color == "white" || color == "black" ? @color = color : color = "NA"
+    color == "white" || color == "black" ? @color = color : @color = "NA" 
+    @pieces = []
   end
 
+  def get_pieces(board)
+    board.each do |file|
+      file.each do |piece|
+        if piece != nil  
+          if piece.color == @color then @pieces.push(piece) end
+        end
+      end
+    end
+  end
+
+  def take_turn
+
+  end
 end
