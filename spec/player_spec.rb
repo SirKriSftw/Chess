d319588f665d5c9e319ff2c53eab3747d0741354
Kristian Perez <kpere192@fiu.edu>
2024-05-01 13:03:17 -0400

    Made checkmate? scholars mate test a little cleaner

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,13 +5,14 @@ describe Player do
     describe "checkmate?" do
         it "Scholar's mate" do
             board = Board.new()
-            board.players[0].take_turn("e2 e4")
-            board.players[1].take_turn("e7 e5")
-            board.players[0].take_turn("f1 c4")
-            board.players[1].take_turn("b8 c6")
-            board.players[0].take_turn("d1 h5")
-            board.players[1].take_turn("g8 f6")
-            board.players[0].take_turn("h5 f7")
+            moves = ["e2 e4", "e7 e5", "f1 c4", "b8 c6", "d1 h5", "g8 f6", "h5 f7"]
+            curr_player = 0
+            index = 0
+            7.times do
+                board.players[curr_player].take_turn(moves[index])
+                index += 1
+                curr_player = 1 - curr_player
+            end
             board.print_board
             expect(board.players[1].checkmate?).to eql true
         end
